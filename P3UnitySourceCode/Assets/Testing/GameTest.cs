cac787a4cffe34b468c0caccf64a529c2cd04b31
Judah92 <70044136+Judah92@users.noreply.github.com>
2024-04-20 20:33:13 -0500

    UnitySC
    Unity Source Code for Project

---
+++ P3UnitySourceCode/Assets/Testing/GameTest.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GameTest
+{
+   
+    [Test]
+    public void GameTestSimplePasses()
+    {
+        
+    }
+
+   
+    [UnityTest] 
+    public IEnumerator TestSceneName()
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestButtonsPress()//May end up using several different varients to test all the different buttons.
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestCharacterKnowledgePersists()
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestCorrectAnswerClick()
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestWrongAnswerClick()
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestAddscoreClick()
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestScoreAppends()
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestScorePersists()
+    {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator TestScoreFileCreates()
+    {
+        yield return null;
+    }
+
+
+}
